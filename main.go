37dd39017ff40b722df7e5523fe3c954608ddf51
ovisannn <ovi.sanjaya11@gmail.com>
2021-12-21 08:33:48 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"fmt"
 
-	mongoDriver "capstone-project-API/drivers/mongoDB"
+	_mongoDriver "disspace/drivers/mongoDB"
 )
 
 func main() {
 	var ctx = context.Background()
-	db, err := mongoDriver.Connect_to_db(ctx)
+	db, err := _mongoDriver.Connect_to_db(ctx)
 	fmt.Println(db, err)
 
 }
