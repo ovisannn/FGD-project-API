10d79518ea844fb10f0c561ff19978b64b269acc
Geraldio Ramadhan Safitri <geralmers01@gmail.com>
2022-01-05 08:05:22 +0800

    fixing code

--- controllers/votes/request/vote_request.go
+++ controllers/votes/request/vote_request.go
@@ -12,12 +12,6 @@ type Vote struct {
 	TimeLike    time.Time `json:"time_like" bson:"time_like"`
 }
 
-type VoteParam struct {
-	ReferenceID string    `json:"reference_id" bson:"reference_id"`
-	Status      int       `json:"status" bson:"status"`
-	TimeLike    time.Time `json:"time_like" bson:"time_like"`
-}
-
 func (request *Vote) ToDomain() *votes.Domain {
 	return &votes.Domain{
 		UserID:      request.UserID,
